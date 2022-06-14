3746924d2f2b0ff98fc32c7a40c3083b8d720e49
Rohan Singh <rjsdebug@gmail.com>
2022-06-14 12:31:36 -0400

    randomly select x% of dataset, hash image tensor to remember which ones were selected and swapped

---
+++ hashtable.py
@@ -0,0 +1,21 @@
+import torch
+import random
+images = {}
+
+# x have id 1, swapped to 4
+# y has id 2, swapped to 5
+# z has id 9, swapped to 7
+targets = [1,2,9]
+x = torch.randn(5,5)
+y = torch.randn(5,5)
+z = torch.randn(5,5)
+w = torch.randn(5,5)
+train_data = [x,y,z]
+
+images[hash(x)]=(1,4)
+images[hash(y)]=(2,5)
+images[hash(z)]=(9,7)
+
+indices = list(range(0,100))
+random.shuffle(indices)
+print(indices[:5])
--- swapper.py
+++ swapper.py
@@ -4,7 +4,8 @@ from torch import nn
 import torchvision
 import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
-
+import numpy as np
+import random
 # if gpu is available, use GPU
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -22,8 +23,46 @@ test_dataset = torchvision.datasets.MNIST(root='./data',train=False
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
 batch_size=batch_size, shuffle=True)
 
-example = train_dataset[26]
-print(train_dataset.targets)
+# take in x 0-9, output a y 0-9, where x!=y
+def swap_label(x):
+    y = np.random.randint(0,10)
+    if x==y:
+        y=swap_label(x)
+    return torch.tensor(y)
+
+# create the swapped set
+PROPORTION_SWAP = 0.0001
+swap_set_size = int(len(train_dataset) * PROPORTION_SWAP)
+#pure_set_size = len(train_dataset) - swap_set_size
+#swap_set, pure_set = torch.utils.data.random_split(train_dataset, [swap_set_size, pure_set_size])
+
+#print('swap data set size:', len(swap_set))
+#print('pure data set size:', len(pure_set))
+
+# iterate through swap_set and perform swap, remembering results with hashtable
+swap_table = {} # tensor : (old, new)
+indices = list(range(0,len(train_dataset)))
+random.shuffle(indices)
+for i in range(0,swap_set_size):
+   index = indices[i]
+   if i==0:
+       index=0
+   old = train_dataset.targets[index]
+  # print("old: "+str(old))
+   new = swap_label(old)
+  # print("new: "+str(new))
+   swap_table[hash(train_dataset[index])]=(old.item(),new.item())
+   train_dataset.targets[index] = new
+
+print("Done")
+print(swap_table)
+
+img=iter(torch.utils.data.DataLoader(train_dataset)).next()
+print(img)
+print(train_dataset.targets[0])
+#print(train_dataset.targets[27])
+#print(swap_label(train_dataset.targets[0]))
+
 """
 feature = example[0]
 label = example[1]
